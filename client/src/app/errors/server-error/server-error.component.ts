63e0f48b3023221ce49ab49a76055ef0b01c9c2b
Anh Le <aab5yzz@mmm.com>
2021-02-01 21:54:48 -0700

    End of Section 7

---
+++ client/src/app/errors/server-error/server-error.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-server-error',
+  templateUrl: './server-error.component.html',
+  styleUrls: ['./server-error.component.css']
+})
+export class ServerErrorComponent implements OnInit {
+  error: any;
+
+  constructor(private router: Router) { 
+    //Router can be access inside of the constructor only
+    const navigation = this.router.getCurrentNavigation();
+    this.error = navigation?.extras?.state?.error;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
